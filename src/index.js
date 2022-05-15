7e88b85b0982119f1eb4cb3f7a7e366f3a4465c9
uzzal71 <uzzalroy.acm@gmail.com>
2022-05-16 00:09:33 +0600

    Create unorder list using react create element or jsx code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 ReactDOM.render(
-  React.createElement("h1", { style: { color: "blue" } }, "Heyyyyy React"),
+  React.createElement(
+    "ul", 
+    null,
+    React.createElement("li", null, "Uzzal Kumar Roy"),
+    React.createElement("li", null, "Nasir Uddin"),
+    React.createElement("li", null, "Sujon Roy"),
+    React.createElement("li", null, "Juwel Rana"),
+    ),
   document.getElementById("root")
 );
 
